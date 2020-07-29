cad719ee724e94f020749a822a919eaddec45b8e
Hari-Nikesh-cce <67548927+Hari-nikesh-cce@users.noreply.github.com>
2020-07-29 19:48:39 +0530

    Update Gotomeeting.py

--- Gotomeeting.py
+++ Gotomeeting.py
@@ -1,13 +1,18 @@
+#We are importing webbrowser to access the webbrowser
 import webbrowser
 
+#setting the Directory for chrome browser
 chrome='C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+#This the url prefix for the gotomeeting.
 u='https://app.gotomeeting.com/?meetingId='
 
 code=input("Enter the access code or paste the link:").strip()
 v=""
-
+#To seperate digits from the string we are using this for loop.
 for i in code:
-    if i.isdigit():
+    if i.isdigit():  #this seperates the digits.
         v+=i
-url=u+v
+url=u+v   #Concadenating the digit and url prefix to a variable.
 webbrowser.get(chrome).open(url)
+#Opening Chrome webbrowser with that url
